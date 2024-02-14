e7d31a757d7a300f5e168e8721fba5c867da98b4
msi <shakirov.vitaliy@gmail.com>
2024-02-14 18:15:08 +0300

    First pull

--- reqsoft/settings.py
+++ reqsoft/settings.py
@@ -132,12 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        pathlib.Path.joinpath(BASE_DIR, "static"),
-    ]
-else:
-    STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    pathlib.Path.joinpath(BASE_DIR, "static"),
+]
+# STATIC_ROOT = 'static/'
 
 
 MEDIA_URL = 'media/'
