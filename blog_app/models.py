927c1d422cfea2b41010455695233a55bfa33292
msi <shakirov.vitaliy@gmail.com>
2024-02-21 17:35:43 +0300

    Third pull

--- blog_app/models.py
+++ blog_app/models.py
@@ -45,7 +45,7 @@ class Article(models.Model):
             """
             Список статей (SQL запрос с фильтрацией для страницы списка статей)
             """
-            return self.get_queryset().select_related('author', 'category').filter(status='published')
+            return self.get_queryset().select_related('author', 'category').prefetch_related('ratings', 'views').filter(status='published')
 
         def detail(self):
             """
